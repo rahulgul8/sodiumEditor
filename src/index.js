dd5c81744d51f1700b22afb591670c4a8541e877
K Rahul <rahul.k@centrico.tech>
2020-03-02 17:01:27 +0530

    Exporting Editor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 import WeEditor from './WeEditor';
 
-ReactDOM.render(<WeEditor />, document.getElementById('root'));
+export default WeEditor;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
